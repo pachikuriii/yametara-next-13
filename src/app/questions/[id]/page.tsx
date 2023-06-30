2c1205461285653528cbfa14a69f05a885fc9c0e
pachikuriii <stakahashi0728@gmail.com>
2023-06-30 11:10:17 +0900

    feat: 型を仮で追加

--- src/app/questions/[id]/page.tsx
+++ src/app/questions/[id]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import useSWR from "swr";
 import PrevNextButtons from "@/components/questions/prevNextButtons";
 
@@ -7,27 +6,33 @@ const Page = ({ params }: { params: { id: string } }) => {
   if (params === null) {
     return <h1>Loading...</h1>;
   }
+
+  const onClick = () => {
+    console.log(params.id);
+  };
+
   return (
     <>
       <h1 className="text-2xl text-center">{params.id}</h1>
-
-      <Content id={params.id} />
-
+      <button onClick={onClick} className="text-2xl">
+        {params.id}
+      </button>
       <PrevNextButtons id={params.id} />
+      {/* <Content id={params.id} /> */}
     </>
   );
 };
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-function Content({ id }: { id: string }) {
-  const { data, error } = useSWR(
-    "https://jsonplaceholder.typicode.com/todos/" + id,
-    fetcher
-  );
-  if (error) return <h1>Failed to load</h1>;
-  if (!data) return <h1>Loading...</h1>;
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
-}
+// function Content({ id }: { id: string }) {
+//   const { data, error } = useSWR(
+//     "https://jsonplaceholder.typicode.com/todos/" + id,
+//     fetcher
+//   );
+//   if (error) return <h1>Failed to load</h1>;
+//   if (!data) return <h1>Loading...</h1>;
+//   return <pre>{JSON.stringify(data, null, 2)}</pre>;
+// }
 
 export default Page;
