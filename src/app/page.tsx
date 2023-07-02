c25bc235ab6a86e99845df0ba829d76bbe15a963
pachikuriii <stakahashi0728@gmail.com>
2023-07-02 23:56:36 +0900

    文言追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl text-center">yametara</h1>
-
+      <p>会社を辞めてからの保険/税金の手続き…</p>
+      <p>何を、どこへ、いつまでに？</p>
       <Link href="/questions/1">はじめる</Link>
     </>
   );
