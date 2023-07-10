ef92f76b0cb8e597674fa997f4a3726ab0dac7ad
pachikuriii <stakahashi0728@gmail.com>
2023-07-10 22:49:20 +0900

    不要なファイルの混入消した
    まとめる

--- src/redux/actions.tsx
+++
@@ -1,7 +0,0 @@
-import actionTypes from "@/type/action.type";
-export const addRetirementDate = (date: string) => {
-  return {
-    type: actionTypes.ADD_RETIREMENT_DATE,
-    payload: date,
-  };
-};
--- src/type/action.type.tsx
+++
@@ -1,5 +0,0 @@
-const actionTypes = {
-  ADD_RETIREMENT_DATE: "ADD_RETIREMENT_DATE",
-};
-
-export default actionTypes;
