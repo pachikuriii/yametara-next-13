bf0c2406909f1baa6db3ce0dc1d48159efc6ba74
pachikuriii <stakahashi0728@gmail.com>
2023-08-31 22:27:39 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import { useSelector, useDispatch } from 'react-redux'
-import Footer from '../components/top/footer'
+import Footer from '@/components/top/footer'
 import { RootState } from '@/redux/reducers'
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <h1 className='text-3xl text-center'>yametara</h1>
 
-      <Link href='/questions/1'>はじめる</Link>
+      <Link href={{ pathname: '/questions/[id]', query: { id: 1 } }}>はじめる</Link>
 
       <Footer />
     </>
--- src/app/questions/[id]/page.tsx
+++ src/app/questions/[id]/page.tsx
@@ -2,9 +2,9 @@
 import Q1 from '../../../components/questions/q1'
 
 const Page = ({ params }: { params: { id: string } }) => {
-  // if (params === null) {
-  //   return <h1>Loading...</h1>
-  // }
+  if (params === null) {
+    return <h1>Loading...</h1>
+  }
 
   return <>{params.id === '1' && <Q1 id={params.id} />}</>
 }
